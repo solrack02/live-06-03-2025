0f7b34162a4dab11cc706f85fe454c717de8d91b
Carlos A. <carlosvsk@gmail.com>
2025-03-06 15:45:44 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,171 +1,175 @@
-// ---------- import React Packs
-import React from "react";
-import * as RN from "react-native";
 
-// ---------- import Variables Pack
-import { create } from "zustand";
 
-// ---------- import Local Tools
-import { mapElements } from "./tools/base/project/mapElements";
-import * as functions from "./tools/base/functions";
-import * as Elements from "./tools/base/Elements";
-import { Project } from "./tools/base/project";
-import * as jsvals from "./tools/base/jsvals";
-import { props } from "./tools/base/props";
-import * as customs from "./tools/customs";
-import * as stls from "./tools/base/stls";
-import { tools } from "./tools";
+          // ---------- import React Packs
+          import React from 'react';
+          import * as RN from 'react-native';
 
-// ---------- set Caps Inputs
-const currRoute = "sc1";
+          // ---------- import Variables Pack
+          import { create } from 'zustand';
 
-let args: any = [];
+          // ---------- import Local Tools
+          import { mapElements } from './tools/base/project/mapElements';
+          import * as functions from './tools/base/functions';
+          import * as Elements from './tools/base/Elements';
+          import { Project } from './tools/base/project';
+          import * as jsvals from './tools/base/jsvals';
+          import { props } from './tools/base/props';
+          import * as customs from './tools/customs';
+          import * as stls from './tools/base/stls';
+          import { tools } from './tools';
 
-const screens = [
-  (...args: any) => (
-    <Elements.Screen3
-      pass={{
-        pathScreen: "sc1",
+          // ---------- set Caps Inputs
+          const currRoute = 'sc1';
 
-        styles: [
-          '{ backgroundColor: "red", alignItems: "center", justifyContent: "center", flex: 1 }',
-        ],
+          let args: any = [];
 
-        screenElements: [
-          (...args: any) => (
-            <Elements.DynView
-              pass={{
-                elementsProperties: [
-                  '{ disabled: "$var_all.cond1", onHoverIn: "$var_all.func1", children: "$var_all.Comp1" }',
-                ],
+          const screens = [
+            
+        
 
-                styles: [
-                  '{ backgroundColor: "$var_all.colors.primary", width: 20, height: 20 }',
-                ],
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"sc1",
 
-                functions: [
-                  async (...args) =>
-                    functions.funcGroup({
-                      args,
-                      pass: {
-                        arrFunctions: [
-                          (...args) => {
-                            // ---------- get Function from A_Project Scope
-                            return tools.goTo("sc2");
-                          },
-                        ],
-                        trigger: "on press",
-                      },
-                    }),
-                ],
-                childrenItems: [() => <></>],
+          styles:['{ backgroundColor: "red", alignItems: "center", justifyContent: "center", flex: 1 }'],
 
-                args,
-              }}
-            />
-          ),
-          (...args: any) => (
-            <Elements.FlatList2
-              pass={{
-                elementProperties: [
-                  '{ItemSeparatorComponent: "$var_all.Comp1"}',
-                ],
+          screenElements:[
+        
 
-                pData: "all.lists.lt1",
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{ disabled: "$var_all.cond1", onHoverIn: "$var_all.func1", children: "$var_all.Comp1" }'],
 
-                itemElements: [
-                  (...args) => {
-                    return (
-                      <RN.Text>
-                        Adicione os campos que quer mostrar aqui.
-                      </RN.Text>
-                    );
-                  },
-                ],
+            styles:['{ backgroundColor: "$var_all.colors.primary", width: 120, height: 40, marginTop: 60, alignItems: "center", justifyContent: "center" }'],
 
-                args,
-              }}
-            />
-          ),
-        ],
+            functions:[async (...args) =>
+ functions.funcGroup({ args, pass:{
+ arrFunctions: [
+        (...args) => {
+          // ---------- get Function from A_Project Scope
+          return tools.goTo("sc2");
+        }
+        ]
+ , trigger: 'on press'
+}})],            childrenItems:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
 
-        functions: [() => {}],
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
 
-        args,
-      }}
-    />
-  ),
+          children: [
+            'Ir p/ Tela 2'
+          ],
 
-  (...args: any) => (
-    <Elements.Screen3
-      pass={{
-        pathScreen: "sc2",
+          args,
 
-        styles: [
-          {
-            backgroundColor: "#101",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-          },
-        ],
+        }}/>],
 
-        screenElements: [
-          () => {
-            const textStyle = {
-              fontSize: 20,
-              color: "#fff2",
-              textAlign: "center",
-              maxWidth: 200,
-              // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
-            };
+            args,
+          }}/>
+        , (...args:any) => <Elements.FlatList2 pass={{
+          elementProperties: [
+            '{ItemSeparatorComponent: "$var_all.Comp1"}'
+          ],
 
+          pData: 'all.lists.lt1',
+
+          itemElements: [
+            (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            '$arg_name'
+          ],
+
+          args,
+
+        }}/>
+          ],
+
+          args,
+        }}/>],
+
+          functions:[()=>{}],
+
+          args,
+        }}/>, 
+
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"sc2",
+
+          styles:[
+              {
+                backgroundColor: '#101',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+              }
+              ],
+
+          screenElements:[() => {
+                  const textStyle = {
+                    fontSize: 20,
+                    color: '#fff2',
+                    textAlign:'center',
+                    maxWidth: 200,
+                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
+                  };
+
+                  return (
+                    <RN.Text style={textStyle}>
+                      {'Adicione Elementos nessa tela!'}
+                    </RN.Text>);
+                }],
+
+          functions:[()=>{}],
+
+          args,
+        }}/>
+          ];
+
+          const initCt = () => (
+ {
+ 'all': { 
+'colors': { 
+'primary': "#fff", 'secondary': "red" } 
+, 
+'cond1': false, 
+'func1': (nativeEvent)=>console.log(nativeEvent), 
+'Comp1': <RN.Text style={{color: "darkred"}}>---</RN.Text>, 
+'lists': { 'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}] } 
+, 'name': "Name:" } 
+ 
+} 
+ );
+          const initObj = initCt();
+          // console.log(initObj);
+
+          const arrInitFuncs = [
+            ()=>{}
+          ];
+
+          export const useRoutes = create(() => ({ currRoute }));
+          export const useData = create(() => initObj);
+
+          // ---------- set Main Component
+          export const Router = () => {
             return (
-              <RN.Text style={textStyle}>
-                {"Adicione Elementos nessa tela!"}
-              </RN.Text>
+              <Project
+                configData={{
+                  screens,
+                  initCt,
+                  arrInitFuncs,
+                }}
+              />
             );
-          },
-        ],
-
-        functions: [() => {}],
-
-        args,
-      }}
-    />
-  ),
-];
-
-const initCt = () => ({
-  all: {
-    colors: {
-      primary: "yellow",
-      secondary: "red",
-    },
-    cond1: false,
-    func1: (nativeEvent) => console.log(nativeEvent),
-    Comp1: <RN.Text style={{ color: "darkred" }}>---</RN.Text>,
-    lists: { lt1: [{ name: "carlos" }, { name: "juan" }, { name: "leandro" }] },
-  },
-});
-const initObj = initCt();
-// console.log(initObj);
-
-const arrInitFuncs = [() => {}];
-
-export const useRoutes = create(() => ({ currRoute }));
-export const useData = create(() => initObj);
-
-// ---------- set Main Component
-export const Router = () => {
-  return (
-    <Project
-      configData={{
-        screens,
-        initCt,
-        arrInitFuncs,
-      }}
-    />
-  );
-};
+          };
+        
