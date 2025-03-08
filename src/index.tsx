16c800bd258900edffc8dfa7e43ae223a3b22b14
Carlos A. <carlosvsk@gmail.com>
2025-03-08 17:51:13 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -211,7 +211,7 @@
 'Comp1': <RN.Text style={{color: "darkred"}}>---</RN.Text>, 
 'lists': { 'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}] } 
 , 
-'name': "Name:", 'dataToSet': {name: "Carlos"} } 
+'name': "Name:", 'dataToSet': {name: "Carlos", phone: "1111"} } 
  
 } 
  );
