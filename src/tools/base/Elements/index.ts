91dd6c7e36cc0f5acd404bef1d955cb1a8458efa
Carlos A. <carlosvsk@gmail.com>
2025-03-08 12:34:57 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/index.ts
+++ src/tools/base/Elements/index.ts
@@ -1,7 +1,2 @@
 
-export { DynView } from './DynView';
-export { Screen3 } from './Screen3';
-export { ImageBox } from './ImageBox';
-export { Text } from './Text';
-export { Loader } from './Loader';
-export { IptTxtEdit } from './IptTxtEdit';
+undefined
