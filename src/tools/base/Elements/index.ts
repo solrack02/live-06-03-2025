657de3b58a8afdf1c700c0090bdba41a48d7ba6a
Carlos A. <carlosvsk@gmail.com>
2025-03-08 12:26:13 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/index.ts
+++ src/tools/base/Elements/index.ts
@@ -6,3 +6,4 @@ export { Screen3 } from './Screen3';
 export { ImageBox } from './ImageBox';
 export { Text } from './Text';
 export { FlatList2 } from './FlatList2';
+export { Loader } from './Loader';
