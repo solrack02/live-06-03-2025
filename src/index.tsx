7364bd5c7e2d90f48707962e3d279fbd0398dcda
Carlos A. <carlosvsk@gmail.com>
2025-03-08 17:49:43 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -181,7 +181,12 @@
 
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
- arrFunctions: [()=>{}]
+ arrFunctions: [async (...args) =>
+        functions.firebase.setDocTool({ args, pass:{
+  arrRefStrings: [`tests`],
+            arrPathData: [`all.dataToSet`],
+            arrFuncs: [()=>{}],
+        }})]
  , trigger: 'on press'
 }})],            childrenItems:[() =><></>],
 
