892152ebc5e9fece10ca1eccc2709d9832466f46
Carlos A. <carlosvsk@gmail.com>
2025-03-08 12:39:40 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -138,11 +138,7 @@
         }}/>, (...args:any) => <Elements.IptTxtEdit pass={{
           propsArray: [{}],
 
-          stylesArray: [{
-                color: 'black',
-                fontSize: 14,
-                // fontSize: '20px',<= #ATTENTION: Native ERROR! No string!
-              }],
+          stylesArray: ['{ backgroundColor: "lightblue" }'],
 
           editPath: [" "],
 
