a2975294afb3de836ac16d044840be6f8822a4fa
Carlos A. <carlosvsk@gmail.com>
2025-03-13 12:19:53 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/DynView.tsx
+++ src/tools/base/Elements/DynView.tsx
@@ -96,9 +96,7 @@ export const DynView = (props: Tprops) => {
 
   // ---------- set Render
   if (!sttTypeFunc)
-    return (
-      <ScrollView {...allProps}>{mapElements(childrenItems, args)}</ScrollView>
-    );
+    return <View {...allProps}>{mapElements(childrenItems, args)}</View>;
 
   if (sttTypeFunc === 'on press') {
     allProps.children = mapElements(childrenItems, args);
@@ -110,7 +108,5 @@ export const DynView = (props: Tprops) => {
   }
 
   if (sttTypeFunc === 'on init')
-    return (
-      <ScrollView {...allProps}>{mapElements(childrenItems, args)}</ScrollView>
-    );
+    return <View {...allProps}>{mapElements(childrenItems, args)}</View>;
 };
